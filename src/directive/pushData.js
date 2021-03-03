f6200b0cce27c3015f662e367c2ac0a3234d0d46
guowf <wenfeng.guo2@zhenai.com>
2021-03-03 14:56:40 +0800

    ''

--- src/directive/pushData.js
+++ src/directive/pushData.js
@@ -3,13 +3,12 @@
  * @desc 埋点，点击元素上报信息
  * @example
  * ```vue
- * <div v-pushData="{accessPoint:'123',accessPointDesc:'测试push按钮',extData:{id:456}}">
+ * <button v-pushData="{accessPoint:'123',accessPointDesc:'测试push按钮',extData:{id:456}}">
  * ```
  */
 import pushData from '@/assets/js/monitor'
 export default {
     inserted(el, binding) {
-        console.log(binding)
         el.handler = () => {
             pushData(binding.value.accessPoint, binding.value.accessPointDesc, binding.value.extData)
         }
